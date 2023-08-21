1a24d323f27bdd9375ecd46ff1dc7ec0fc9a32c5
pravtz <ederson.ufrgs@gmail.com>
2023-08-20 23:59:32 -0300

    feat: request and response helpers

---
+++ handler/response.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func sendError(ctx *gin.Context, code int, msg string) {
+	ctx.Header("Content-type", "application/json")
+	ctx.JSON(code, gin.H{
+		"message":   msg,
+		"errorCode": code,
+	})
+}
+func sendSuccess(ctx *gin.Context, op string, data interface{}) {
+	ctx.Header("Content-type", "application/json")
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("operation %s successfull", op),
+		"data":    data,
+	})
+}
